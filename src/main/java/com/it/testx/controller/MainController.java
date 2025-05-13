79998fc69e90a436256c2fda8596317936e67ab9
yeran <yeran250506@gmail.com>
2025-05-13 15:48:48 +0800

    update user

--- src/main/java/com/it/testx/controller/MainController.java
+++ src/main/java/com/it/testx/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.it.testx.controller;
 
+import com.it.testx.annotation.AuthCheck;
 import com.it.testx.common.BaseResponse;
 import com.it.testx.common.ResultUtils;
+import com.it.testx.constant.UserConstant;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ public class MainController {
     /**
      * 健康检查
      */
+//    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     @GetMapping("/health")
     public BaseResponse<String> health() {
         return ResultUtils.success("ok");
