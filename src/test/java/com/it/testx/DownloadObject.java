0e22c9e51fc90b2c532fd2d607b39a026ba67045
yeran <yeran250506@gmail.com>
2025-05-20 10:38:55 +0800

    add GCS

---
+++ src/test/java/com/it/testx/DownloadObject.java
@@ -0,0 +1,64 @@
+package com.it.testx;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class DownloadObject {
+  @Test
+  public void downloadObject() throws IOException {
+    // The ID of your GCP project
+    String projectId = "testx-459207";
+
+    // The ID to give your GCS bucket
+    String bucketName = "testx_bucket";
+
+//    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+    Storage storage = StorageOptions.newBuilder()
+            .setProjectId(projectId)
+            .setCredentials(credentials)
+            .build()
+            .getService();
+
+    // The ID of your GCP project
+    // String projectId = "your-project-id";
+
+    // The ID of your GCS bucket
+    // String bucketName = "your-unique-bucket-name";
+
+    // The ID of your GCS object
+    // String objectName = "your-object-name";
+
+    // The path to which the file should be downloaded
+    // String destFilePath = "/local/path/to/file.txt";
+
+    // The ID of your GCS object
+    String objectName = "UploadObject.java";
+
+    // The path to your file to upload
+    String destFilePath = "/Users/ye/Desktop/Code/TestX/src/test/java/com/it/testx/Down.java";
+
+//    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+
+    Blob blob = storage.get(BlobId.of(bucketName, objectName));
+    blob.downloadTo(Paths.get(destFilePath));
+
+    System.out.println(
+        "Downloaded object "
+            + objectName
+            + " from bucket name "
+            + bucketName
+            + " to "
+            + destFilePath);
+  }
+}
---
+++ src/test/java/com/it/testx/UploadObject.java
@@ -0,0 +1,70 @@
+package com.it.testx;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class UploadObject {
+  @Test
+  public void uploadObject() throws IOException {
+    // The ID of your GCP project
+    String projectId = "testx-459207";
+
+    // The ID to give your GCS bucket
+    String bucketName = "testx_bucket";
+
+//    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+    Storage storage = StorageOptions.newBuilder()
+            .setProjectId(projectId)
+            .setCredentials(credentials)
+            .build()
+            .getService();
+
+
+    // The ID of your GCP project
+    // String projectId = "your-project-id";
+
+    // The ID of your GCS bucket
+    // String bucketName = "your-unique-bucket-name";
+
+    // The ID of your GCS object
+     String objectName = "UploadObject.java";
+
+    // The path to your file to upload
+     String filePath = "/Users/ye/Desktop/Code/TestX/src/test/java/com/it/testx/UploadObject.java";
+
+//    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    BlobId blobId = BlobId.of(bucketName, objectName);
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+
+    // Optional: set a generation-match precondition to avoid potential race
+    // conditions and data corruptions. The request returns a 412 error if the
+    // preconditions are not met.
+    Storage.BlobWriteOption precondition;
+    if (storage.get(bucketName, objectName) == null) {
+      // For a target object that does not yet exist, set the DoesNotExist precondition.
+      // This will cause the request to fail if the object is created before the request runs.
+      precondition = Storage.BlobWriteOption.doesNotExist();
+    } else {
+      // If the destination already exists in your bucket, instead set a generation-match
+      // precondition. This will cause the request to fail if the existing object's generation
+      // changes before the request runs.
+      precondition =
+          Storage.BlobWriteOption.generationMatch(
+              storage.get(bucketName, objectName).getGeneration());
+    }
+    storage.createFrom(blobInfo, Paths.get(filePath), precondition);
+
+    System.out.println(
+        "File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
+  }
+}
